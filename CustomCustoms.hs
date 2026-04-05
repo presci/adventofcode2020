ccdf49277509c3d321a8f9d4ddfe8685898f11ee
prasad iyer <p.c.iyer@gmail.com>
2026-04-05 18:13:20 -0500

    Create CustomCustoms.hs

---
+++ CustomCustoms.hs
@@ -0,0 +1,19 @@
+module CustomCustoms
+    (customCustoms)
+where
+
+import Data.List (sort, nub)
+== https://adventofcode.com/2020/day/6
+
+mylines :: [String] -> [String]
+mylines [] = []
+mylines ("":xs) = mylines xs
+mylines xs =
+    let (line, rest) = span (/= "") xs
+    in unwords line: mylines rest
+
+customCustoms::IO()
+customCustoms = do
+    contents <- readFile "customCustoms.txt"
+    let answers = sum $ map length $ map (sort . nub . (filter (/= ' ')) ) $ mylines $ lines contents
+    print answers
